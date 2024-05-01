64b9207f4d19060fd42e8661d183de9899df5dc1
TanThanhLe <tanthanhcr95@gmail.com>
2024-05-01 18:35:39 +0700

    Using Python set intersection() method to intersect two or more sets

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 if __name__ == "__main__":
-  print("Python Set Intersection")
+  # Using Python set intersection() method to intersect two or more sets
+
+  """
+  """
+
+  s1 = {"Python", "Java","C++"}
+  s2 = {"C#", "Java", "C++"}
